a2ef72599c997f4ad292e6067e9471a171f80a18
Nathan Galindo <nathan.zebedee@gmail.com>
2024-07-18 15:35:40 -0500

    edit sdk examples

--- ts-sdk/get-bank-by-symbol.ts
+++ ts-sdk/get-bank-by-symbol.ts
@@ -1,8 +1,6 @@
 import { Connection } from "@solana/web3.js";
 import { MarginfiClient, getConfig } from '@mrgnlabs/marginfi-client-v2';
 import { NodeWallet } from "@mrgnlabs/mrgn-common";
-import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
-import { BorshAccountsCoder } from "@coral-xyz/anchor";
 
 const RPC_ENDPOINT = "https://rpc.ironforge.network/mainnet?apiKey=01HTYZW4C7W74CTK8N8XN2GMR5";
 
@@ -12,7 +10,7 @@ async function main() {
     const config = getConfig("production");
     const client = await MarginfiClient.fetch(config, wallet, connection);
 
-    const bankSymbol = "PYUSD";
+    const bankSymbol = "GUAC";
     const bank = client.getBankByTokenSymbol(bankSymbol);
     console.log(bank)
 }
